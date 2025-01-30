0c9c7a75e4c681ed511e46c807019331054759d9
Kay Zou <zouzhuoy@shernet.sheridancollege.ca>
2025-01-30 10:49:22 -0500

    Sound stuff

--- Assets/InClass/enableDisable.cs
+++ Assets/InClass/enableDisable.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class enableDisable : MonoBehaviour
 {
     public GameObject menu;
-    public SpriteRenderer sr;
+    public AudioSource clickSound;
+    //public SpriteRenderer sr;
     public enableDisable script;
+    public AudioClip clip;
+    bool isPlaying;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,8 +23,8 @@ public class enableDisable : MonoBehaviour
         {
             sr.enabled = false;
             menu.SetActive(false);
-            
             //script.enabled = false;
+
 
         }
         if (Input.GetKeyDown(KeyCode.Space))
@@ -29,6 +32,15 @@ public class enableDisable : MonoBehaviour
             sr.enabled = true;
             menu.SetActive(true);
             //script.enabled = true;
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if (clickSound.isPlaying == false)
+            {
+                //clickSound.Play();
+                clickSound.PlayOneShot(clip);
+            }
+
         }
         
     }
